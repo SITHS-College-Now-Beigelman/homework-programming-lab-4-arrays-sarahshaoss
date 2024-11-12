cfab469373587017966322a9edb40abfd750fd1b
Sarah <sarahs389@nycstudents.net>
2024-11-12 17:49:10 -0500

    Create Shao_Sarah_Homework_4.cpp

---
+++ Shao_Sarah_Homework_4.cpp
@@ -0,0 +1,19 @@
+//Sarah Shao
+//11/12/2024
+//Homework 4
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double myArray[8];
+    double num1, num2, num3, num4, num5, num6, num7, num8;
+
+    cout << "Enter 8 numbers separrated by spaces for the array: "<< endl;
+    cin >> num1, num2, num3, num4, num5, num6, num7, num8;
+
+    myArray[8] = num1, num2, num3, num4, num5, num6, num7, num8;
+
+}
